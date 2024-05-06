a5a2be812efb64395e16f7488e762231aa6884fa
elfbe <huynhngocnhon2004@gmail.com>
2024-05-07 05:01:27 +0700

    fix

--- CarSalesVENV/CarSales/views.py
+++ CarSalesVENV/CarSales/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.apps import apps
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+def connectDB():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("carsales-admin-key.json")
+        firebase_admin.initialize_app(cred, {
+            "databaseURL": "https://projectname-xxxxxxxxxxx-rtdb.firebaseio.com/" #Your database URL
+        })
+    dbconn = db.reference("CarsList")
+    return dbconn
 
 def carslist(request):
-    return render(request, 'carslist.html')
+    cars = []
+    dbconn = connectDB()
+    tblCars = dbconn.get()
+    for key, value in tblCars.items():
+        cars.append({"id": value["ID"], "name": value["Name"], "year": value["Year"], "price": value["Price"]})
+    return render(request, 'carslist.html', {'cars':cars})
